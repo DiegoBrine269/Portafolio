87501498c66741d73b7da592c043a9c47b08f99f
DiegoBrine269 <diiegoman@hotmail.com>
2021-04-14 15:33:26 -0500

    Index casi listo, faltan animaciones con wow js

--- deploy/build/js/bundle.js
+++ deploy/build/js/bundle.js
@@ -1,19 +1,22 @@
 
-
-
+const main = document.querySelector('main')
+let menu = document.querySelector('.navbar')
+let imgPhone = document.querySelector('#img-phone')
+let imgTablet = document.querySelector('#img-tablet')
 
 document.addEventListener("DOMContentLoaded", function(event) {
+    
+    
 
     // Look for .hamburger
     let hamburger = document.querySelector(".hamburger");
-    let menu = document.querySelector('.navbar')
-    let imgPhone = document.querySelector('#img-phone')
-    let imgTablet = document.querySelector('#img-tablet')
+    
+
 
     //Si es mayor, que se meuestre el menu
     if (window.matchMedia("(min-width: 767px)").matches){
         mostrarMenu()
-        //cambiarImagen()
+        cambiarImagen()
     }
 
 
@@ -21,6 +24,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //Cada que cambia el tamaño, cambia las clases del botón y del menú
     window.addEventListener('resize', function(){
+        margenSuperior = main.style.marginTop
+
+        //Mostrar u ocultar imagen
+        let dispositivo = 'phone'
+        let anchoPantalla = tamVentana()[0]
+        
+
+        if (anchoPantalla < 768)
+            dispositivo = 'phone'
+        else
+            dispositivo = 'tablet'
+
+
+        if(dispositivo === 'phone'){
+            imgPhone.classList.remove('img-oculta')
+            imgTablet.classList.add('img-oculta')
+        }
+        else if(dispositivo === 'tablet'){
+            imgPhone.classList.add('img-oculta')
+            imgTablet.classList.remove('img-oculta')
+        }
+            
+            
+
         if (window.matchMedia("(max-width: 767px)").matches){ 
             //Aquí había un bug, cuando se abría el menú y leugo se cambiaba a tablet, y luego a phone, desaparecía el menú, pero el boyón seguí en x
             ocultarMenu()
@@ -28,11 +55,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
             if(hamburger.classList.contains("is-active")){
                 hamburger.classList.toggle("is-active")
             }
+            
 
         }
         else{
             mostrarMenu()
         }
+
+        //Cambiamos la imagen que deseamos mostrar, en función al tamaño de pantalla
+
+
     
     }, true)
 
@@ -49,25 +81,73 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     })
 
-    function mostrarMenu(){
-        menu.classList.add('visible')
-        menu.classList.remove('oculto')
 
-    }
 
-    function ocultarMenu(){
-        menu.classList.remove('visible')
-        menu.classList.add('oculto')
-    }
-
-    function cambiarImagen(){
-        imgPhone.classList.toggle('img-oculta')
-        imgTablet.classList.toggle('img-oculta')
-
-    }
+    scrollNav()
 
 
 });
 
+function mostrarMenu(){
+    menu.classList.add('visible')
+    menu.classList.remove('oculto')
 
+    //Cambiamos el margen del main para que se mueva cuando abrimos el menú        
+    main.style.marginTop = '25rem'  
 
+}
+
+function ocultarMenu(){
+    menu.classList.remove('visible')
+    menu.classList.add('oculto')
+
+    //Cambiamos el margen del main para que se mueva cuando abrimos el menú    
+    main.style.marginTop = '0' 
+}
+
+function cambiarImagen(){
+    imgPhone.classList.toggle('img-oculta')
+    imgTablet.classList.toggle('img-oculta')
+
+}
+
+function tamVentana() {
+    var tam = [0, 0];
+    if (typeof window.innerWidth != 'undefined')
+    {
+      tam = [window.innerWidth,window.innerHeight];
+    }
+    else if (typeof document.documentElement != 'undefined'
+        && typeof document.documentElement.clientWidth !=
+        'undefined' && document.documentElement.clientWidth != 0)
+    {
+      tam = [
+          document.documentElement.clientWidth,
+          document.documentElement.clientHeight
+      ];
+    }
+    else   {
+      tam = [
+          document.getElementsByTagName('body')[0].clientWidth,
+          document.getElementsByTagName('body')[0].clientHeight
+      ];
+    }
+    return tam;
+}
+
+function scrollNav(){
+    const enlaces = menu.querySelectorAll('a')
+
+    
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e){
+            e.preventDefault()
+            
+            const seccion = document.querySelector(e.target.attributes.href.value)
+
+            seccion.scrollIntoView({
+                behavior: 'smooth'
+            })
+        })
+    })
+}
